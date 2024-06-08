21802bafa86f989512342110d71b678c77e4048d
abdullah2422 <abdullahmohammed1am@gmail.com>
2024-06-08 14:27:58 +0000

    abdoo

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-# Configuration file
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
 
 include stdlib
 
@@ -11,6 +11,16 @@ file_line { 'SSH Private Key':
   append_on_no_match => true
 }
 
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
 file_line { 'Deny Password Auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
@@ -18,3 +28,13 @@ file_line { 'Deny Password Auth':
   replace            => true,
   append_on_no_match => true
 }
+
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
